55f46a3e62a0a6d50e4d2d7ecfbf6a19655e1f7d
SISI <schwermc@my.erau.edu>
2024-02-03 21:00:17 -0700

    The Start of M01 PR02

---
+++ CS_M01_PR02/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+
+    private float vInput;
+    private float hInput;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        /*
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        */
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+
+        _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+}
